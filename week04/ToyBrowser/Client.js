ce1947c77c053c8d5ebd1062627dc7c6c5042946
xlwu25 <xlwu25@gmail.com>
2020-07-31 00:06:02 +0800

    + HTML解析

--- week04/ToyBrowser/Client.js
+++ week04/ToyBrowser/Client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const parser = require("./Parser.js");
 
 class Request{
     constructor(options){
@@ -207,6 +208,7 @@ void async function (){
     });
 
     let response = await request.send();
-    console.log(response);
+    let dom = parser.parseHTML(response.body);
+    console.log(dom);
 }();
 
